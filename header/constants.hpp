7ecf62c4af4c8a754c776fc0aba10fcbbf0629d5
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-04-15 22:39:42 +0200

    feat(utils): add constants and types files

---
+++ header/constants.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+inline std::vector<std::pair<int,int>> directions={{-1,0},{0,-1},{0,1},{1,0}}; //U,L,R,D
---
+++ header/types.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+#include <queue>
+#include "graph.hpp"
+
+// aliases
+
+using ll=long long;
+using i64 = std::int64_t;
+using u64 = std::uint64_t;
+
+using MinHeap=std::priority_queue<int>;
+
